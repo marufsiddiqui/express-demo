18cf4a67dc57b9ec4d42b284f8113b417b51d3c8
Maruf Siddiqui <maruf@newscred.com>
2013-05-21 14:19:52 +0600

    Initial passport integration. need much more tweak

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -8,17 +7,39 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , passport = require('passport')
-  , LocalStrategy = require('passport-local')
+  , mongoose = require('mongoose')
+  , LocalStrategy = require('passport-local').Strategy
   , path = require('path');
 
 var app = express();
 
-// Setup passport local strategy
-passport.use(new LocalStrategy(
-  function (username, password, done) {
-    
-  }
-));
+// connect to mongodb
+mongoose.connect('mongodb://localhost/test');
+
+// get the db connection handle
+var db = mongoose.connection;
+
+// Add some error handler
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// just to make sure we are connected to db
+db.on('open', function () {
+  console.log('Connected to DB');
+});
+
+// lets define a scheme to use with mongoose
+var userSchema = mongoose.Schema({
+  username: String,
+  password: String
+});
+
+// add a validate password method to schema
+userSchema.methods.validPassword = function (password) {
+  return !!(password === this.password);
+};
+
+// alright now register a user model via the userSchema
+var User = mongoose.model('User', userSchema);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -28,8 +49,40 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+
+// add express cookieParser and session
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'keyboard cat' }));
+
+// now add the passport initialization and session
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+// for managing session in passport the following code is required
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+//setup local strategy
+passport.use(new LocalStrategy(function (username, password, done) {
+    User.findOne({username: username}, function (err, user) {
+      if (err) return done(err);
+
+      if (!user) return done(null, false, {message: 'Incorrect username.'});
+
+      if (!user.validPassword(password)) return done(null, false, {message: 'Incorrect password.'});
+
+      return done(null, user);
+    });
+  }
+));
 
 // development only
 if ('development' == app.get('env')) {
@@ -39,9 +92,18 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/login', user.login);
-app.post('/login', user.postLogin);
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+    failureFlash: false
+  })
+);
+app.get('/home', function (req, res) {
+  res.send('This is home');
+});
 
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
